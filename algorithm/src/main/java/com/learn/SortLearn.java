3eaaa1b7a7c6055b8e3a46aafa2390a39e1f75a3
17338152891@163.com <17338152891@163.com>
2018-10-23 21:17:53 +0800

    添加排序项目

--- algorithm/src/main/java/com/learn/SortLearn.java
+++ algorithm/src/main/java/com/learn/SortLearn.java
@@ -1,6 +1,119 @@
 package com.learn;
 
+import java.util.*;
+
 public class SortLearn {
+
+    public static List<Integer> sortByQ(List<Integer> datas){
+        if(datas.size() == 1){
+            return datas;
+        }
+        if(datas.size() == 0){
+            return new LinkedList<Integer>();
+        }
+        int shuzhi = datas.get(0);
+
+        LinkedList<Integer> bigs = new LinkedList();
+        LinkedList<Integer> simal = new LinkedList();
+        for (int data: datas) {
+            if(data < shuzhi){
+                simal.add(data);
+            }else if(data > shuzhi){
+                bigs.add(data);
+            }else {
+
+            }
+        }
+        List<Integer> sortBigs = sortByQ(bigs);
+        List<Integer> sortSimal = sortByQ(simal);
+        sortBigs.add(shuzhi);
+        sortBigs.addAll(sortSimal);
+        return sortBigs;
+    }
+
+    public static List<Integer> sortByM(List<Integer> datas){
+        Integer[] toArray = datas.toArray(new Integer[datas.size()]);
+        int size = toArray.length;
+        for(int i = 0;i < size ;i++){
+            for(int j = i + 1; j < size; j++){
+                if(toArray[i] < toArray[j]) {
+                    int tmp = toArray[i];
+                    toArray[i] = toArray[j];
+                    toArray[j] = tmp;
+                }
+
+            }
+        }
+        return Arrays.asList(toArray);
+    }
+
+
+
+    public static List<Integer> sortByG(List<Integer> datas){
+        if(datas.size() == 1){
+            return datas;
+        }
+        if(datas.size() == 0){
+            return new ArrayList<Integer>();
+        }
+        if(datas.size() == 2){
+            List<Integer> res = new ArrayList<Integer>(2);
+            if(datas.get(0) < datas.get(1)){
+                res.add(datas.get(0));
+                res.add(datas.get(1));
+            }else {
+                res.add(datas.get(1));
+                res.add(datas.get(0));
+            }
+            return res;
+        }
+        int size = datas.size();
+        int i = size/2;
+
+        List<Integer> simal = new ArrayList<Integer>(i);
+        List<Integer> bigs = new ArrayList<Integer>(datas.size() - i);
+
+        for(int j = 0;j < size; j++){
+            if(j < i){
+                simal.add(datas.get(j));
+            }else {
+                bigs.add(datas.get(j));
+            }
+        }
+        List<Integer> sortBigs = sortByG(bigs);
+        List<Integer> sortSimal = sortByG(simal);
+        List<Integer> hebing = hebing(sortSimal, sortBigs);
+        return hebing;
+    }
+
+    public static List<Integer> hebing(List<Integer> list1, List<Integer> list2){
+        Iterator<Integer> iterator = list2.iterator();
+        List<Integer> all = new LinkedList<Integer>();
+        Integer from2 = null;
+        for(Integer i : list1){
+            if(from2 != null && from2 < i){
+                all.add(from2);
+                from2 = null;
+            }
+            while ((from2 == null || from2 < i) && iterator.hasNext()){
+                from2 = iterator.next();
+                if(from2 < i){
+                    all.add(from2);
+                    from2 = null;
+                }else{
+                    break;
+                }
+            }
+            all.add(i);
+        }
+        if(from2 != null){
+            all.add(from2);
+        }
+        while (iterator.hasNext()){
+            all.add(iterator.next());
+        }
+        return all;
+    }
 
 
 
--- algorithm/src/test/java/com/learn/TestSortLearn.java
+++ algorithm/src/test/java/com/learn/TestSortLearn.java
@@ -1,5 +1,62 @@
 package com.learn;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
 public class TestSortLearn {
-    
+    List<Integer> datas = null;
+
+
+    @Before
+    public void init(){
+        Random random = new Random();
+        datas = new LinkedList<Integer>();
+        for(int i = 0; i < 100000; i++){
+            datas.add(random.nextInt(100000));
+        }
+        //System.out.println(datas);
+    }
+
+    @Test
+    public void testSortByQ() {
+        long start = System.currentTimeMillis();
+        List<Integer> res = SortLearn.sortByQ(datas);
+        long end = System.currentTimeMillis();
+        System.out.println("q ---> " + (end - start));
+        //System.out.println(res);
+    }
+
+    @Test
+    public void testSortByM() {
+        long start = System.currentTimeMillis();
+        List<Integer> res = SortLearn.sortByM(datas);
+        long end = System.currentTimeMillis();
+        System.out.println("m ---> " + (end - start));
+        //System.out.println(res);
+    }
+
+    @Test
+    public void testSortByG() {
+        long start = System.currentTimeMillis();
+        List<Integer> res = SortLearn.sortByG(datas);
+        long end = System.currentTimeMillis();
+        System.out.println("g ---> " + (end - start));
+
+    }
+
+    @Test
+    public void testSortByJ() {
+        long start = System.currentTimeMillis();
+        Collections.sort(datas);
+        long end = System.currentTimeMillis();
+        System.out.println("j ---> " + (end - start));
+        //System.out.println(res);
+    }
+
+
 }
